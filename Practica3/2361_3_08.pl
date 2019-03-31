d417092dc233fe44d28f40f6a92771204bdefe8f
Javier Muñoz Haro <javier.munnozharo@estudiante.uam.es>
2019-03-31 17:51:50 +0200

    Hecho hasta el ejercicio 4

--- Practica3/2361_3_08.pl
+++ Practica3/2361_3_08.pl
@@ -1,12 +1,24 @@
+% ejercicio 1
+
 duplica([], []).
 duplica([X|R], [X, X|S]) :- duplica(R, S).
+
+% ejercicio 2
+
+concatena([], L, L). 
+concatena([X|L1], L2, [X|L3]) :-
+    concatena(L1, L2, L3).
 
 invierte([], []).
 invierte([X|L], R) :- 
 	invierte(L, S),
 	append(S, [X], R).
 
+% ejercicio 3
+
 palindromo[L] :- invierte(L, L)
+
+% ejercicio 4
 
 divide(L, 0, [], L)
 divide([X|L], N, [X|L1], L2) :- 
