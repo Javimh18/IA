33e4b88325b60055992aa4b7c81d031377afcc91
Javier Muñoz Haro <javier.munnozharo@estudiante.uam.es>
2019-04-12 19:08:44 +0200

    Funciona el ej6

--- Practica3/2361_3_08.pl
+++ Practica3/2361_3_08.pl
@@ -16,11 +16,11 @@ invierte([X|L], R) :-
 
 % ejercicio 3
 
-palindromo[L] :- invierte(L, L)
+palindromo[L] :- invierte(L, L).
 
 % ejercicio 4
 
-divide(L, 0, [], L)
+divide(L, 0, [], L).
 divide([X|L], N, [X|L1], L2) :- 
     N > 0,
     M is N-1,
@@ -31,3 +31,40 @@ divide([X|L], N, [X|L1], L2) :-
 divide(L, N, R, S) :-
     append(R, S, L),
     length(R, N).
+
+ % ejercicio 5
+
+aplasta([], []).
+aplasta([X|L], LA) :-
+    aplasta(X, X1),
+    aplasta(L, L1) ,
+    concatena(X1, L1, LA).        
+aplasta(X, [X]). 
+
+ % ejercicio 6
+
+primos(1, [_]).
+primos(N, [N]):-
+   	N < 3.
+		
+primos(N, L):-
+    N > 1,
+    primos_rec(N, 2, L).
+
+% necesitoo un caso baseeeee
+primos_rec(1, _, []).
+primos_rec(N, F, [F|L]):-
+    0 is N mod F,
+    A is N / F,
+	primos_rec(A, F, L).
+
+primos_rec(N, F, L):-
+    next_factor(N, F, NF), 
+    primos_rec(N, NF, L).
+    
+next_factor(_, 2, 3).
+next_factor(N, F, NF):-
+	F<sqrt(N),
+	NF is F+2.
+next_factor(N, _, N):-
+    N > 2.
